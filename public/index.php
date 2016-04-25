ce84812ca4ef31e98d7c44a5cefb3ab53d54c08b
Darren Taylor <darren.taylor@planysdigital.com>
2016-04-25 14:28:58 +0100

    Add CSRF protection

--- public/index.php
+++ public/index.php
@@ -40,6 +40,27 @@ catch(RoutingException $exception) {
 # Get instance of the controller via Dependency Injenction Container
 $controller = $container->get(sprintf('\App\Controllers\%s', $router->getController()));
 
+if(getenv('REQUEST_METHOD') == "POST")
+{
+    $headers = getallheaders();
+
+    if(!array_key_exists('X-XSRF-TOKEN', $headers))
+    {
+        header('HTTP/1.0 403 Forbidden');
+        exit();       
+    }
+
+    if($headers['X-XSRF-TOKEN'] != $_COOKIE['XSRF-TOKEN'])
+    {
+        header('HTTP/1.0 403 Forbidden');
+        exit();          
+    }
+}
+else
+{
+    setcookie('XSRF-TOKEN', md5(uniqid(rand(), true)), time() + 3600, '/');
+}
+
 # Call the controller/action
 $contents = $controller->{$router->getAction()}();
 
